ecd4ca011076fa1ad6075178313f1d4ad9acf102
leecolin0607 <1017519632@qq.com>
2025-04-24 20:13:11 +0800

    定时任务修改

--- src/backend/bisheng/worker/main.py
+++ src/backend/bisheng/worker/main.py
@@ -7,10 +7,10 @@ setup_llm_caching()
 
 bisheng_celery = Celery('bisheng', include=['bisheng.worker'])
 bisheng_celery.config_from_object('bisheng.worker.config')
-bisheng_celery.conf.beat_schedule = {
-    'check_model_status_task': {
-        'task': 'bisheng.worker.model.check_models.check_model_status_task',
-        'schedule': crontab(minute='*/1'),
-    }
-}
+# bisheng_celery.conf.beat_schedule = {
+#     'check_model_status_task': {
+#         'task': 'bisheng.worker.model.check_models.check_model_status_task',
+#         'schedule': crontab(minute='*/120'),
+#     }
+# }
 
