28b2ba4f440f42e287e9e5e636bf231155e70ad1
zzy <flowerheap@163.com>
2025-06-13 18:49:14 +0800

    fix: 修复样式bug

--- src/frontend/src/pages/BuildPage/flow/FlowChat/GuideQuestions.tsx
+++ src/frontend/src/pages/BuildPage/flow/FlowChat/GuideQuestions.tsx
@@ -34,7 +34,9 @@ const GuideQuestions = forwardRef(({ locked, chatId, onClick, bottom }, ref) => 
     if (locked || !words.length) return null
     return (
         <div className="relative">
-            <div className={`absolute left-0 bottom-${bottom || 0}`}>
+            <div className={`absolute left-0`}
+                style={{ bottom: `${(bottom || 0) + 28}px` }}
+            >
                 {window.workflow_flow?.name === 'AI助手' ? null : <p className="text-gray-950 text-sm mb-2 bg-[rgba(255,255,255,0.8)] rounded-md w-fit px-2 py-1">
                     {t('chat.recommendationQuestions')}
                 </p>}
