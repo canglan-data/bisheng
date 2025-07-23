8ccf040e41343245fa6171c7db75b3b44dcb686d
zzy <flowerheap@163.com>
2025-07-23 18:34:55 +0800

    feat: AVATAR

--- src/frontend/platform/src/pages/BuildPage/skills/l2Edit.tsx
+++ src/frontend/platform/src/pages/BuildPage/skills/l2Edit.tsx
@@ -187,7 +187,7 @@ export default function l2Edit() {
                     <div className="w-full overflow-hidden transition-all px-1">
                         <div className="mt-4">
                             <Label htmlFor="name">{t('skills.avatar')}</Label>
-                            <Avator value={logo} className="mt-2" onChange={uploadAvator}><SkillIcon className="bg-primary w-9 h-9 rounded-sm" /></Avator>
+                            <Avator value={logo ? __APP_ENV__.BASE_URL + logo : ''} className="mt-2" onChange={uploadAvator}><SkillIcon className="bg-primary w-9 h-9 rounded-sm" /></Avator>
                         </div>
                         <div className="mt-4">
                             <Label htmlFor="name">{t('skills.skillName')}</Label>
